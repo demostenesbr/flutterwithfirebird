624713658ea86e3c20567d3ce253bffbb94950a5
demostenesbr <demostenes@demostenes.com.br>
2024-02-01 16:25:41 -0300

    Feat: insert dotenv on the project

---
+++ lib/signin/screens/LoginScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// DotEnv dotenv = DotEnv() is automatically called during import.
+// If you want to load multiple dotenv files or name your dotenv object differently, you can do the following and import the singleton into the relavant files:
+// DotEnv another_dotenv = DotEnv()
+
+Future main() async {
+  // To load the .env file contents into dotenv.
+  // NOTE: fileName defaults to .env and can be omitted in this case.
+  // Ensure that the filename corresponds to the path in step 1 and 2.
+  await dotenv.load(fileName: ".env");
+  //...runapp
+}
